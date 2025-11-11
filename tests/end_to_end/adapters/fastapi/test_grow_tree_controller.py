faf42e809805bceabb2af6c0cf1fe4af6b16b029
1bouillonbaptiste <1bouillonbaptiste@gmail.com>
2025-11-11 13:36:08 +0100

    feat(fastapi): test grow a tree with branches.

--- tests/end_to_end/adapters/fastapi/test_grow_tree_controller.py
+++ tests/end_to_end/adapters/fastapi/test_grow_tree_controller.py
@@ -11,7 +11,7 @@ from treenity.core.models.branch import Branch
 
 def test_can_grow_a_new_tree(test_client: TestClient):
     branch_repository = InMemoryBranchRepository()
-    split_strategy = DualSplitStrategy(split_probability=0.1)
+    split_strategy = DualSplitStrategy(split_probability=0.0)
 
     test_client.app.dependency_overrides[get_branch_repository] = lambda: branch_repository
     test_client.app.dependency_overrides[get_split_strategy] = lambda: split_strategy
@@ -24,3 +24,23 @@ def test_can_grow_a_new_tree(test_client: TestClient):
     assert branch_repository.get_by_id(uuid.UUID(client_response.result)) == Branch(
         id=uuid.UUID(client_response.result), length=2
     )
+
+
+def test_can_grow_a_new_tree_with_branches(test_client: TestClient):
+    branch_repository = InMemoryBranchRepository()
+    split_strategy = DualSplitStrategy(split_probability=1.0)
+
+    test_client.app.dependency_overrides[get_branch_repository] = lambda: branch_repository
+    test_client.app.dependency_overrides[get_split_strategy] = lambda: split_strategy
+
+    response = test_client.post("/generate-tree", json={"iterations": 6})
+
+    assert response.status_code == 201
+
+    client_response = GenerateTreeResponse.model_validate(response.json())
+
+    result_tree = branch_repository.get_by_id(uuid.UUID(client_response.result))
+
+    assert result_tree.id == uuid.UUID(client_response.result)
+    assert result_tree.length == 6
+    assert len(result_tree.children_ids) == 2
