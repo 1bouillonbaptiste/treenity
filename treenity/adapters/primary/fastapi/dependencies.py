d19b9571cce6ca76328f2f3deb6ebd2b4a82d793
1bouillonbaptiste <1bouillonbaptiste@gmail.com>
2025-11-11 14:00:14 +0100

    refactor(fastapi): interface types for dependencies.

--- treenity/adapters/primary/fastapi/dependencies.py
+++ treenity/adapters/primary/fastapi/dependencies.py
@@ -3,24 +3,24 @@
 from fastapi import Depends
 from starlette.requests import Request
 
-from treenity.adapters.secondary.gateways.dual_split_strategy import DualSplitStrategy
-from treenity.adapters.secondary.gateways.in_memory_branch_repository import InMemoryBranchRepository
+from treenity.core.gateways.branch_repository import BranchRepository
+from treenity.core.gateways.split_strategy import SplitStrategy
 from treenity.core.grow_tree import GrowTreeUseCase
 
 
-def get_branch_repository(request: Request) -> InMemoryBranchRepository:
+def get_branch_repository(request: Request) -> BranchRepository:
     """Get the in-memory branch repository."""
     return request.app.state.branch_repository
 
 
-def get_split_strategy(request: Request) -> DualSplitStrategy:
+def get_split_strategy(request: Request) -> SplitStrategy:
     """Get the dual split strategy."""
     return request.app.state.split_strategy
 
 
 def get_grow_tree_use_case(
-    branch_repository: InMemoryBranchRepository = Depends(get_branch_repository),  # noqa: B008, `Depends` is a valid default
-    split_strategy: DualSplitStrategy = Depends(get_split_strategy),  # noqa: B008, `Depends` is a valid default
+    branch_repository: BranchRepository = Depends(get_branch_repository),  # noqa: B008, `Depends` is a valid default
+    split_strategy: SplitStrategy = Depends(get_split_strategy),  # noqa: B008, `Depends` is a valid default
 ) -> GrowTreeUseCase:
     """Get the grow tree use case."""
     return GrowTreeUseCase(
